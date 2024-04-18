9894ed7851f4fac03f724dceeaeb31ede0e578b1
harsh02it <harshitsavani@gmail.com>
2024-04-17 23:48:33 -0400

    Complete the logic for the upgraded tic tac toe game

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const cell_selector = document.querySelectorAll(".cell");
 const reset_button = document.querySelector("#reset");
 const winner_message = document.querySelector("#message");
 let currentPlayer = "X";
+
+let currentTurn = 0;
+let moveHistory = [];
+
 const result = [
   [0, 1, 2],
   [3, 4, 5],
@@ -16,29 +20,52 @@ const result = [
 cell_selector.forEach((cell) => {
   cell.addEventListener("click", function (event) {
     if (!event.target.textContent.trim()) {
+      const elementId = event.target.id;
+      const number = elementId.slice(5);
+      moveHistory.push(number);
       event.target.textContent = currentPlayer;
-      console.log(event.target.id, currentPlayer);
       currentPlayer = currentPlayer === "X" ? "O" : "X";
-    }
-    for (let i = 0; i < result.length; i++) {
-      let [a, b, c] = result[i];
-      if (
-        cell_selector[a].textContent &&
-        cell_selector[a].textContent === cell_selector[b].textContent &&
-        cell_selector[a].textContent === cell_selector[c].textContent
-      ) {
-        winner_message.textContent =
-          "Winner is " + cell_selector[a].textContent;
-        return;
+
+      let winnerFound = false;
+      currentTurn++;
+      console.log(moveHistory);
+
+      if (currentTurn >= 7) {
+        const cellToDisappear = moveHistory.shift();
+        const disappearingCell = document.getElementById(
+          "cell-" + cellToDisappear
+        );
+        disappearingCell.textContent = "";
+      }
+
+      for (let i = 0; i < result.length; i++) {
+        let [a, b, c] = result[i];
+        if (
+          cell_selector[a].textContent &&
+          cell_selector[a].textContent === cell_selector[b].textContent &&
+          cell_selector[a].textContent === cell_selector[c].textContent
+        ) {
+          alert("Winner is " + cell_selector[a].textContent);
+          winnerFound = true;
+          break;
+        }
+      }
+
+      if (winnerFound) {
+        resetGame();
       }
     }
   });
 });
 
-reset_button.addEventListener("click", function () {
+function resetGame() {
   cell_selector.forEach((cell) => {
     cell.textContent = "";
     currentPlayer = "X";
     winner_message.textContent = "";
   });
+}
+
+reset_button.addEventListener("click", function () {
+  resetGame();
 });
